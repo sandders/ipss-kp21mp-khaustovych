b9d17f2ebe9b8b96f72bd4bb8f90544ae7535ea5
Oleksandr Khaustovych <oleksandr.khaustovych@wesoftyou.com>
2022-11-15 19:13:38 +0200

    Add tests

---
+++ sem_9_lab_1/test.py
@@ -0,0 +1,34 @@
+import unittest, tempfile, shutil
+from os import path
+from indexes import InvertedIndex, ForwardIndex
+
+
+class TestIndexes(unittest.TestCase):
+
+    def setUp(self):
+        # Create a temporary directory
+        self.test_dir = tempfile.mkdtemp()
+        self.file_pass_1 = path.join(self.test_dir, 'test1.txt')
+        self.file_pass_2 = path.join(self.test_dir, 'test2.txt')
+        with open(self.file_pass_1, "w") as file:
+            file.write("Woops! I have deleted all content")
+        with open(self.file_pass_2, "w") as file:
+            file.write("The content was deleted")
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
+
+    def index_search(self, index):
+        index.build_index()
+        assert index.search('all') == [self.file_pass_1]
+        assert index.search('was') == [self.file_pass_2]
+        assert set(index.search('content')) == {self.file_pass_1, self.file_pass_2}
+
+    def test_inverted_index(self):
+        index = InvertedIndex(self.test_dir)
+        self.index_search(index)
+
+    def test_forvard_index(self):
+        index = ForwardIndex(self.test_dir)
+        self.index_search(index)
